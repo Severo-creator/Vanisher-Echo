c2c8abff2ed3ef391e834567c9f4aa8e70302db5
João Mello <joao.mello.100@ufrn.edu.br>
2024-07-11 19:08:46 -0300

    Adição de background com efeito parallax
    - Adição de background com efeito parallax
    - Adição de altura mínima da câmera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,7 +4,8 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target; 
     public float smoothSpeed = 0.125f; 
-    public Vector3 offset; 
+    public Vector3 offset;
+    public float minY;
 
     void LateUpdate()
     {
@@ -12,6 +13,11 @@ public class CameraFollow : MonoBehaviour
         if (target != null )
         {
             Vector3 desiredPosition = target.position  + offset;
+
+            if (desiredPosition.y < minY)
+            {
+                desiredPosition.y = minY;
+            }
 
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
 
